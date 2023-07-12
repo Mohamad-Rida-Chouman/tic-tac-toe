977a49a08ff722b54d06d8e8e51cb0688c861844
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-07-12 08:21:52 +0300

    created gameboard

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let gameBoard = document.querySelector('#gameBoard')
+
+let statusDisplay = document.querySelector('#status')
+
+let emptyCells = [
+    '', '', '', '', '', '', '', '', ''
+]
+
+function createBoard() {
+    emptyCells.forEach((cell, index) => {
+        let cellElement = document.createElement('div')
+        cellElement.classList.add('square')
+    })
+}
+
