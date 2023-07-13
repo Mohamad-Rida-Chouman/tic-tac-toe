61eead6dae7505637157dc78d081742042843b78
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-07-13 14:05:01 +0300

    added draw condition

--- script/script.js
+++ script/script.js
@@ -10,6 +10,10 @@ let turn = 'circle'
 
 statusDisplay.textContent = 'Circle, Commence!'
 
+let moves = 0
+
+let winnerFound = 0
+
 function createBoard() {
 
     emptyCells.forEach((cell, index) => {
@@ -19,7 +23,7 @@ function createBoard() {
         cellElement.classList.add('square')
         
         cellElement.id = index
-        
+
         cellElement.addEventListener('click', mark)
         
         gameBoard.append(cellElement)
@@ -36,35 +40,48 @@ function mark (e) {
     turn = turn === 'circle' ? 'cross' : 'circle'
     
     statusDisplay.textContent = 'Dare to make a move, ' + turn + '?'
-    
+
     e.target.removeEventListener('click', mark)
+
     checkScore()
+
 }
 
 function checkScore() {
-    
-    let squares = document.querySelectorAll('.square')
-    let winCombos = [
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6]
-    ]
-    
-    winCombos.forEach(array => {
-        let winCircle = array.every(cell => squares[cell].firstChild?.classList.contains('circle'))
-        if (winCircle) {
-            statusDisplay.textContent = 'Well played, circle. Well played...'
-            squares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
-        }
-    })
 
-    winCombos.forEach(array => {
-        let winCross = array.every(cell => squares[cell].firstChild?.classList.contains('cross'))
-        if (winCross) {
-            statusDisplay.textContent = 'Magnificient game, cross!'
+        moves = moves + 1
+        console.log(moves)
+
+        let squares = document.querySelectorAll('.square')
+        let winCombos = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+        ]
+        
+
+        winCombos.forEach(array => {
+            let winCircle = array.every(cell => squares[cell].firstChild?.classList.contains('circle'))
+            if (winCircle) {
+                winnerFound = 1
+                statusDisplay.textContent = 'Well played, circle. Well played!'
+                squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                console.log('circle')
+            }
+        })
+
+        winCombos.forEach(array => {
+            let winCross = array.every(cell => squares[cell].firstChild?.classList.contains('cross'))
+            if (winCross) {
+                winnerFound = 1
+                statusDisplay.textContent = 'Magnificient game, cross!'
+                squares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                console.log('cross')
+            }
+        })
+
+        if (moves == 9 && winnerFound == 0){
+            statusDisplay.textContent = 'draw'
             squares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return
         }
-    })
-}
+    }
