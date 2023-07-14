f53b137805a66abf22e48976665780288aea6bee
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-07-14 06:17:42 +0300

    fixed player names display in game.html

--- script/script-game.js
+++ script/script-game.js
@@ -6,13 +6,22 @@ let emptyCells = [
     '', '', '', '', '', '', '', '', ''
 ]
 
+player_1 = params.player_1
+player_2 = params.player_2
+
+let toPlay = player_1
+
 let turn = 'circle'
 
-statusDisplay.textContent = 'Circle, Commence!'
+statusDisplay.textContent = player_1.concat(', you are circle. Commence!')
 
 let moves = 0
 
 let winnerFound = 0
+
+let player_1_win = 0
+
+let player_2_win = 0
 
 function createBoard() {
 
@@ -38,8 +47,9 @@ function mark (e) {
     
     e.target.append(markDisplay)
     turn = turn === 'circle' ? 'cross' : 'circle'
+    toPlay = toPlay === player_1 ? player_2 : player_1
     
-    statusDisplay.textContent = 'Dare to make a move, ' + turn + '?'
+    statusDisplay.textContent = 'Dare to make a move, ' + toPlay + '?'
 
     e.target.removeEventListener('click', mark)
 
@@ -64,9 +74,10 @@ function checkScore() {
             let winCircle = array.every(cell => squares[cell].firstChild?.classList.contains('circle'))
             if (winCircle) {
                 winnerFound = 1
-                statusDisplay.textContent = 'Well played, circle. Well played!'
+                statusDisplay.textContent = 'Well played, '+ player_1 +'. Well played!'
                 squares.forEach(square => square.replaceWith(square.cloneNode(true)))
-                console.log('circle')
+                player_1_win = player_1_win + 1
+                console.log(player_1_win)
             }
         })
 
@@ -74,14 +85,14 @@ function checkScore() {
             let winCross = array.every(cell => squares[cell].firstChild?.classList.contains('cross'))
             if (winCross) {
                 winnerFound = 1
-                statusDisplay.textContent = 'Magnificient game, cross!'
+                statusDisplay.textContent = 'Magnificient game, '+ player_2 +'!'
                 squares.forEach(square => square.replaceWith(square.cloneNode(true)))
-                console.log('cross')
+                player_2_win = player_2_win + 1
             }
         })
 
         if (moves == 9 && winnerFound == 0){
-            statusDisplay.textContent = 'draw'
+            statusDisplay.textContent = 'A truce it is then.'
             squares.forEach(square => square.replaceWith(square.cloneNode(true)))
         }
     }
